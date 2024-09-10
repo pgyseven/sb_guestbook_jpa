4440809cf6d07fe92d602b1f3491639c0a4ab663
gayoon-create <kgykgy1123@naver.com>
2024-09-10 09:55:02 +0900

    config : springboot + spring data jpa 프로젝트 생성. 첫 커밋

---
+++ src/main/java/com/kgy/sb_guestbook_jpa/SbGuestbookJpaApplication.java
@@ -0,0 +1,13 @@
+package com.kgy.sb_guestbook_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbGuestbookJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbGuestbookJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kgy/sb_guestbook_jpa/SbGuestbookJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgy.sb_guestbook_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbGuestbookJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
