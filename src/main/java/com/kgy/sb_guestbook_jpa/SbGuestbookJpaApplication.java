f9582fdfe43a29c7ac3cb15de5e5aaad48adba3d
gayoon-create <kgykgy1123@naver.com>
2024-09-20 11:20:53 +0900

    feat : 엔티티의 데이터 추가/변경을 자동으로 감지하는 AuditingEntityListener.class를 활성화시킴

--- src/main/java/com/kgy/sb_guestbook_jpa/SbGuestbookJpaApplication.java
+++ src/main/java/com/kgy/sb_guestbook_jpa/SbGuestbookJpaApplication.java
@@ -2,8 +2,10 @@ package com.kgy.sb_guestbook_jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // AuditingEntityListener.class 활성화시키는 annotation
 public class SbGuestbookJpaApplication {
 
 	public static void main(String[] args) {
