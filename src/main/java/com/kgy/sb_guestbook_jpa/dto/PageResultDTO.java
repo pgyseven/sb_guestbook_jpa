1ca919b64e0a54bcc2778feb56693519ced827cc
gayoon-create <kgykgy1123@naver.com>
2024-09-23 11:03:45 +0900

    doc : 주석 추가

--- src/main/java/com/kgy/sb_guestbook_jpa/dto/PageResultDTO.java
+++ src/main/java/com/kgy/sb_guestbook_jpa/dto/PageResultDTO.java
@@ -10,6 +10,8 @@ import org.springframework.data.domain.Page;
 // 페이징 정보와 페이징된 데이터를 함께 가지고 있는 DTO 클래스
 // <DTO, EN> : 여기에서 DTO는 페이징된 실제 데이터를 의미하고, EN은 페이징 정보를 가지고 있는 entity 객체
 
+// Function<EN, DTO> fn : 엔티티 객체들을 DTO로 변환해주는 함수를 매개변수로 받는다.
+
 public class PageResultDTO<DTO, EN> {
     private List<DTO> dtoList;
 
