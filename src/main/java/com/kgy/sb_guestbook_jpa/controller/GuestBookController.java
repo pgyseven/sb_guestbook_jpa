5b11d8ad4ca3f0fae5835ec90497fcd4ce6a9873
gayoon-create <kgykgy1123@naver.com>
2024-09-23 13:11:06 +0900

    feat : 컨트롤러에 listAll() 생성

--- src/main/java/com/kgy/sb_guestbook_jpa/controller/GuestBookController.java
+++ src/main/java/com/kgy/sb_guestbook_jpa/controller/GuestBookController.java
@@ -1,19 +1,29 @@
 package com.kgy.sb_guestbook_jpa.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kgy.sb_guestbook_jpa.dto.PageRequestDTO;
+import com.kgy.sb_guestbook_jpa.service.GuestBookService;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
 @RequestMapping("/guestbook")
+@RequiredArgsConstructor
 public class GuestBookController {
 
+    private final GuestBookService guestBookService;
+
     @GetMapping("/list")
-    public void listAll() {
-        log.info("listMemos 컨트롤러~~~~~~~~~~");
+    public void listAll(PageRequestDTO pageRequestDTO, Model model) {
+        log.info("listAll 컨트롤러~~~~~~~~~~");
+
+        model.addAttribute("result", guestBookService.getList(pageRequestDTO));
         // = return "/guestbook/list";
     } 
 
