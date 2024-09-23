63cd31a126c038e131ca79d2e6676700f99a302d
gayoon-create <kgykgy1123@naver.com>
2024-09-23 11:02:25 +0900

    feat : 페이징 정보와 페이징된 데이터를 함께 가지고 있는 DTO 클래스 생성

---
+++ src/main/java/com/kgy/sb_guestbook_jpa/dto/PageResultDTO.java
@@ -0,0 +1,19 @@
+package com.kgy.sb_guestbook_jpa.dto;
+
+import java.util.List;
+import java.util.function.Function;
+// Function : 메소드의 자체 클래스.
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
+
+// 페이징 정보와 페이징된 데이터를 함께 가지고 있는 DTO 클래스
+// <DTO, EN> : 여기에서 DTO는 페이징된 실제 데이터를 의미하고, EN은 페이징 정보를 가지고 있는 entity 객체
+
+public class PageResultDTO<DTO, EN> {
+    private List<DTO> dtoList;
+
+    public PageResultDTO(Page<EN> result, Function<EN, DTO> fn) {
+        this.dtoList = result.stream().map(fn).collect(Collectors.toList());
+    }
+}
